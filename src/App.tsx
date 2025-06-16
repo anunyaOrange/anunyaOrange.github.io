85a8c285a3874389da6910d8f89f854e580686af
anunyaOrange <anunyaOrange@proton.me>
2025-06-16 21:15:09 +0700

    add icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import bg1 from '/bg1.jpg'
+import bg2 from '/bg2.jpg'
 import './App.css'
 
 function App() {
@@ -9,14 +10,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>anunyaOrange</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,6 +26,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <img src={bg1} alt="bg1" />
+      <img src={bg2} alt="bg2" />
     </>
   )
 }
